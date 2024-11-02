93de5d7b20934cfd8dd2f76c8dda2e3c2277e30b
AwesomeAnna19 <annaroseyt13@gmail.com>
2024-11-02 16:20:01 +0100

    gunny gun rework (now it should work x))

--- Assets/Scripts/Weapons Scripts/DamageGun.cs
+++ Assets/Scripts/Weapons Scripts/DamageGun.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageGunBehavior : MonoBehaviour
+public class DamageGun : MonoBehaviour
 {
     public float Damage;
     public float BulletRange;
