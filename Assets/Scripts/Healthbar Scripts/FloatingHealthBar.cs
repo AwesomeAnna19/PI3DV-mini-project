b9fc4b81766c7e7f1cd36cfa5e967ee6c4f75c0f
AwesomeAnna19 <annaroseyt13@gmail.com>
2024-11-11 10:04:26 +0100

    comments on the FloatingHealthBar script

--- Assets/Scripts/Healthbar Scripts/FloatingHealthBar.cs
+++ Assets/Scripts/Healthbar Scripts/FloatingHealthBar.cs
@@ -5,10 +5,13 @@ using UnityEngine.UI;
 
 public class FloatingHealthBar : MonoBehaviour
 {
+    // Here I make a slider component from Unity, that acts like a health bar.
     [SerializeField] private Slider slider;
 
+    // Here I make a function that takes the current "health" value and the max "health" value as two arguments.
     public void UpdateHealthBar(float currentValue, float maxValue)
     {
+        // This code then takes the two arguments and divide them by each other, and then puts them as the slider's value.
         slider.value = currentValue / maxValue;
     }
 
